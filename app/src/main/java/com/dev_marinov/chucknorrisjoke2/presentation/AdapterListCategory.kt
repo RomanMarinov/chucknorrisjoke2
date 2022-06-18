4464e7861d2fa4e21b1f79b4d8c85589ec6601af
yu.khmelevskikh <yu.khmelevskikh@robo.cash>
2022-06-18 13:32:13 +0300

    Edited onClickListeners

--- app/src/main/java/com/dev_marinov/chucknorrisjoke2/presentation/AdapterListCategory.kt
+++ app/src/main/java/com/dev_marinov/chucknorrisjoke2/presentation/AdapterListCategory.kt
@@ -1,91 +1,77 @@
 package com.dev_marinov.chucknorrisjoke2.presentation
 
 import android.graphics.Color
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.dev_marinov.chucknorrisjoke2.R
-import com.dev_marinov.chucknorrisjoke2.databinding.RvListBinding
+import com.dev_marinov.chucknorrisjoke2.databinding.ItemCategoryBinding
 import com.dev_marinov.chucknorrisjoke2.presentation.jokes.JokesViewModel
 
 class AdapterListCategory(
-    private val jokesViewModel: JokesViewModel
+    private val jokesViewModel: JokesViewModel,
+    private val mListener: OnItemClickListener
 ) : RecyclerView.Adapter<AdapterListCategory.ViewHolder>() {
 
-    private var arrayList: ArrayList<String> = ArrayList() // массив для хранения категорий
+    private var categories: ArrayList<String> = ArrayList()
 
-    private lateinit var mListener: onItemClickListener // слушатель кликов для нажатия на кн категорий
-
-    interface onItemClickListener {
+    interface OnItemClickListener {
         fun onItemClick(position: Int, clickCategory: String, widthTextViewCategory: Int)
-    }
-
-    fun setOnItemClickListener(listener: onItemClickListener) {
-        mListener = listener
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
 
-        val listItemBinding = RvListBinding.inflate(inflater, parent, false)
+        val listItemBinding = ItemCategoryBinding.inflate(inflater, parent, false)
         return ViewHolder(listItemBinding, mListener)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // передаем в bind значения массива и позицию
-        holder.bind(arrayList[position], position)
+        holder.bind(categories[position], position)
     }
 
-    override fun getItemCount() = arrayList.size
+    override fun getItemCount() = categories.size
 
-    //передаем данные и оповещаем адаптер о необходимости обновления списка
-    fun refreshUsers(arrayList: ArrayList<String>) {
-        Log.e("333", "=adapter arrayList=" + arrayList.size)
-        this.arrayList = arrayList
+    fun refreshCategories(arrayList: ArrayList<String>) {
+        this.categories = arrayList
         notifyDataSetChanged()
     }
 
-    inner class ViewHolder(private val binding: RvListBinding, listener: onItemClickListener) :
-        RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(
+        private val binding: ItemCategoryBinding,
+        listener: OnItemClickListener
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.cardView.setOnClickListener {
+                notifyDataSetChanged()
+                listener.onItemClick(
+                    bindingAdapterPosition,
+                    binding.tvCategory.text.toString(),
+                    binding.tvCategory.width
+                )
+            }
+        }
 
         fun bind(item: String, position: Int) {
-            binding.listItem = item // заполняем tv_category данными
+            binding.category = item
 
-            // выделеный цветом tv_category будет зависеть от наатия
-            binding.tvCategory.setTextColor(
-                if (jokesViewModel.selectedPosition
-                    == position
-                ) Color.parseColor("#FFBB33") else Color.GRAY
-            )
-            binding.cardView.setBackgroundResource(
-                if (jokesViewModel.selectedPosition
-                    == position
-                ) R.drawable.button_turn_off else Color.TRANSPARENT
-            )
+            val textColorResId = if (jokesViewModel.selectedPosition == position)
+                ContextCompat.getColor(binding.root.context, R.color.orange)
+            else Color.GRAY
+
+            val backGroundResId = if (jokesViewModel.selectedPosition == position)
+                R.drawable.button_turn_off
+            else Color.TRANSPARENT
+
+            binding.tvCategory.setTextColor(textColorResId)
+            binding.cardView.setBackgroundResource(backGroundResId)
 
             // Метод executePendingBindings используется, чтобы биндинг не откладывался,
             // а выполнился как можно быстрее. Это критично в случае с RecyclerView.
-            binding.executePendingBindings() // обязательно
-        }
-
-        init {
-            // пишем клик на элемент и фиксируем данных для своих viewModels
-            binding.cardView.setOnClickListener {
-
-                jokesViewModel.selectedPosition = bindingAdapterPosition
-                notifyDataSetChanged()
-
-                // получаем ширину выбранного по клику view
-                jokesViewModel.widthTextViewCategory = binding.tvCategory.width
-
-                // передача интерфейсу выбранной категории
-                listener.onItemClick(
-                    jokesViewModel.selectedPosition,
-                    binding.tvCategory.text.toString(),
-                    jokesViewModel.widthTextViewCategory
-                )
-            }
+            binding.executePendingBindings()
         }
     }
 }
--- app/src/main/java/com/dev_marinov/chucknorrisjoke2/presentation/jokes/JokesFragment.kt
+++ app/src/main/java/com/dev_marinov/chucknorrisjoke2/presentation/jokes/JokesFragment.kt
@@ -2,8 +2,6 @@ package com.dev_marinov.chucknorrisjoke2.presentation.jokes
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,17 +16,14 @@ import com.dev_marinov.chucknorrisjoke2.R
 import com.dev_marinov.chucknorrisjoke2.databinding.FragmentListBinding
 import com.dev_marinov.chucknorrisjoke2.presentation.*
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
 class JokesFragment : Fragment() {
 
-    private lateinit var binding: FragmentListBinding // биндинг для фрагмента
+    private lateinit var binding: FragmentListBinding
 
-    private lateinit var jokesViewModel: JokesViewModel // для сохранения массива категорий
-
-    lateinit var adapterListCategory: AdapterListCategory // адапетр для категорий шуток
+    private lateinit var jokesViewModel: JokesViewModel
 
     private var layoutManager: LinearLayoutManager? = null
 
@@ -40,69 +35,71 @@ class JokesFragment : Fragment() {
         return initInterFace(inflater, container)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewModel()
+        setUpCategoriesRecyclerView()
+        setUpJokeTextView()
+    }
+
     private fun initInterFace(inflater: LayoutInflater, container: ViewGroup?): View {
-        // если уже есть надутый макет, удалить его.
         container?.let { it.removeAllViewsInLayout() }
 
-        // получить экран ориентации
         val orientation = requireActivity().resources.configuration.orientation
-        // раздуть соответствующий макет в зависимости от ориентации экрана
-        binding = if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+        val layoutResId = if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            R.layout.fragment_list
         } else {
-            DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+            R.layout.fragment_list
         }
-        // метод работы с views, adapter, recycler
-        myRecyclerLayoutManagerAdapter()
 
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
         return binding.root
     }
 
-    private fun myRecyclerLayoutManagerAdapter() {
-
+    private fun initViewModel() {
         jokesViewModel = ViewModelProvider(this)[JokesViewModel::class.java]
+    }
 
-        binding.tvTitle.text = resources.getString(R.string.choose)
-
-        adapterListCategory = AdapterListCategory(jokesViewModel)
-        layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-
-        binding.recyclerView.apply {
-            layoutManager = this@JokesFragment.layoutManager
-            adapter = adapterListCategory
-        }
-
-        // слушатель кликов из адаптера чтобы запустить сетевой запрос и получить шутку joke
-        adapterListCategory.setOnItemClickListener(object :
-            AdapterListCategory.onItemClickListener {
+    private fun setUpCategoriesRecyclerView() {
+        val onClickListener = object : AdapterListCategory.OnItemClickListener {
             override fun onItemClick(
                 position: Int,
                 clickCategory: String,
                 widthTextViewCategory: Int
             ) {
-
-                jokesViewModel.onCategoryClicked(clickCategory)// api запрос шутки
-
-                calculateWindowsMetrics(widthTextViewCategory) // смещение view на центр экрана
+                calculateWindowsMetrics(widthTextViewCategory)
             }
-        })
+        }
+
+        val adapter = AdapterListCategory(jokesViewModel, jokesViewModel)
+        layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        binding.recyclerView.apply {
+            layoutManager = this@JokesFragment.layoutManager
+            this.adapter = adapter
+        }
 
         jokesViewModel.categories.observe(viewLifecycleOwner) { categories ->
-            adapterListCategory.refreshUsers(categories)
-            adapterListCategory!!.notifyDataSetChanged()
+            adapter.refreshCategories(categories)
+            adapter!!.notifyDataSetChanged()
 
             jokesViewModel.onCategoryClicked(categories[jokesViewModel.selectedPosition])
 
-            // метод для смещения view на центр экрана
-            calculateWindowsMetrics(jokesViewModel.widthTextViewCategory)
+            calculateWindowsMetrics(jokesViewModel.widthTextViewCategory.value)
         }
+    }
 
-        // наблюдатель для получения шутки ответа от сети
-        jokesViewModel.joke.observe(viewLifecycleOwner) { t ->
-            Log.e("333", "=viewModelListCategory t=" + t)
-            binding.tvJoke.text = t
+    private fun setUpJokeTextView() {
+        jokesViewModel.joke.observe(viewLifecycleOwner) {
+            binding.tvJoke.text = it
+            calculateWindowsMetrics(jokesViewModel.widthTextViewCategory.value)
         }
+    }
 
+    private fun setUpWidthTextViewCategory(){
+        jokesViewModel.widthTextViewCategory.observe(viewLifecycleOwner){
+            calculateWindowsMetrics(it)
+        }
     }
 
     // метод получения размеров view чтобы переместить на центр экрана
