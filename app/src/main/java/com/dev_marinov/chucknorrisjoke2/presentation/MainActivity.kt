a8733856e53225249c21133af5632b107fe8e963
yu.khmelevskikh <yu.khmelevskikh@robo.cash>
2022-06-26 12:30:21 +0300

    Added navigation component

--- app/src/main/java/com/dev_marinov/chucknorrisjoke2/presentation/MainActivity.kt
+++ app/src/main/java/com/dev_marinov/chucknorrisjoke2/presentation/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
 import com.dev_marinov.chucknorrisjoke2.R
 import com.dev_marinov.chucknorrisjoke2.databinding.ActivityMainBinding
 import com.dev_marinov.chucknorrisjoke2.databinding.WindowsAlertdialogExitBinding
@@ -19,44 +20,22 @@ import com.dev_marinov.chucknorrisjoke2.presentation.jokes.JokesFragment
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewModelStatusDialogExit: ViewModelStatusDialogExit
+    private lateinit var viewModelStatusDialogExit: ViewModelStatusDialogExit
     private lateinit var binding: ActivityMainBinding
-    var mySavedInstanceState: Bundle? = null
+    private var mySavedInstanceState: Bundle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        Log.e("333","=MainActivity=")
         mySavedInstanceState = savedInstanceState
 
         viewModelStatusDialogExit = ViewModelProvider(this)[ViewModelStatusDialogExit::class.java]
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        supportActionBar?.hide() // скрыть экшен бар
 
         // при создании макета проверяем статус был ли перед созданием макета открыт диалог
         // если да (true), значит запустим его снова
         if (viewModelStatusDialogExit.status) {
             myAlertDialog()
         }
-
-        val runnable1 = Runnable{ // анимация шарики при старте
-            binding.animationView.playAnimation()
-        }
-        Handler(Looper.getMainLooper()).postDelayed(runnable1, 0)
-        binding.animationView.cancelAnimation()
-
-        val runnable2 = Runnable{ // задержка 1,5 сек перед переходом во FragmentList
-            if(mySavedInstanceState == null) {
-
-            val fragmentList = JokesFragment()
-            val fragmentManager = supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.add(R.id.llFragList, fragmentList)
-            fragmentTransaction.commit()
-            }
-        }
-        Handler(Looper.getMainLooper()).postDelayed(runnable2, 1500)
     }
 
     override fun onBackPressed() {
@@ -87,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             dialog.dismiss()
             dialog.cancel()
         }
-        bindingAlertDialogExit.btYes.setOnClickListener{
+        bindingAlertDialogExit.btYes.setOnClickListener {
             dialog.dismiss()
             finish()
         }
